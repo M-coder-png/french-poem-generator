055766b364dcc30a9b10318a8908751640a63290
M-coder-png <maletshankepana@gmail.com>
2025-12-15 12:00:51 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function generatePoem(event) {
   let apiKey = "d452da0fe2cb4btd0a16e31f1oe44d2f";
 
   let context =
-    "You are a romantic poem expert, and love to write short poems. Your mission is to generate a four line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with SheCodes AI inside a <stong> element at the end of the poem";
+    "You are a romantic poem expert, and love to write short poems. Your mission is to generate a four line poem and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with SheCodes AI inside a <stong> element at the end of the poem";
   let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
